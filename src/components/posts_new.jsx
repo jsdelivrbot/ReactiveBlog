a6d9bcfdc51da545d61f2b6854361b1b71a9d658
Clayton, Casey [GCB-OT] <cc54062@imcnam.ssmb.com>
2018-09-21 09:55:02 -0500

    Working on Redux Form setup

---
+++ src/components/posts_new.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostNew extends Component {
+  render() {
+    return (
+      <div>
+        New Post
+      </div>
+    );
+  }
+}
+
+export default PostNew;
