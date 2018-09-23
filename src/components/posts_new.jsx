dc2ecd1fca8418ea4d0bea32e5f06a9a9b25dc42
Clayton, Casey [GCB-OT] <cc54062@imcnam.ssmb.com>
2018-09-23 01:33:37 -0500

    Redux Form work

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { Field, reduxForm} from 'redux-form';
 
 class PostNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div><input type="text" {...field.input}/></div>
+    );
+  }
+
   render() {
     return (
-      <div>
-        New Post
-      </div>
+      <form>
+        <Field name="title" component={this.renderTitleField} />
+      </form>
     );
   }
 }
 
-export default PostNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostNew);
