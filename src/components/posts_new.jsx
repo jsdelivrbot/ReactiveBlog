df5edec37f3c587d184d72a55e2d8dc42cc57607
Clayton, Casey [GCB-OT] <cc54062@imcnam.ssmb.com>
2018-09-24 16:29:06 -0500

    Added form validation

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -2,21 +2,47 @@ import React, { Component } from 'react';
 import { Field, reduxForm} from 'redux-form';
 
 class PostNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div><input type="text" {...field.input}/></div>
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input type="text" {...field.input} className="form-control"/>
+        {field.meta.error}
+      </div>
     );
   }
 
   render() {
     return (
       <form>
-        <Field name="title" component={this.renderTitleField} />
+        <Field name="title" label="Title" component={this.renderField} />
+        <Field name="categories" label="Categories" component={this.renderField} />
+        <Field name="content" label="Post Content" component={this.renderField} />
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = 'Title is required!';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter some categories!'
+  }
+
+  if (!values.content) {
+    errors.content = 'Content is required!'
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostNew);
